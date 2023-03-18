1301b81f33afed79849630fd333f611542ca6c3d
Sazidur Rahman <rsazidur936@gmail.com>
2023-03-18 14:08:28 +0600

    Basic Output

---
+++ Section5/Basic_Output.cpp
@@ -0,0 +1,15 @@
+//
+// Created by rsazi on 3/18/2023.
+//
+#include <iostream>
+using namespace std;
+int main() {
+
+    cout << "Hello World!" << endl;
+    cout << "Hello" << " World!" << endl;
+    cout << "Hello";
+    cout << " World!\n";
+    cout << "Hello\nOut\nThere";
+
+    return 0;
+}
