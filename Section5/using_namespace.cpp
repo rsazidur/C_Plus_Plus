6cbdaecd5d0fd3bd402d77ddcb88f3299623ffed
Sazidur Rahman <rsazidur936@gmail.com>
2023-03-18 13:35:24 +0600

    Using nameSpace

---
+++ Section5/using_namespace.cpp
@@ -0,0 +1,9 @@
+//
+// Created by rsazi on 3/18/2023.
+//
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello World!" << endl;
+    return 0;
+}
---
+++ Section5/using_qualified_namespace.cpp
@@ -0,0 +1,11 @@
+//
+// Created by rsazi on 3/18/2023.
+//
+#include <iostream>
+using std::cout;
+using std::endl;
+int main() {
+
+    cout << "Hello World!" << endl;
+    return 0;
+}
